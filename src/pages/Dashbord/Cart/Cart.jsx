6a1863f9d423872fa69618754a4427be3470937b
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-11-21 14:22:38 +0600

    add cart and open delete

--- src/pages/Dashbord/Cart/Cart.jsx
+++ src/pages/Dashbord/Cart/Cart.jsx
@@ -1,9 +1,74 @@
+import { MdDelete } from "react-icons/md";
+import useCart from "../../../hooks/useCart/useCart";
+import Swal from "sweetalert2";
+import useAxiox from "../../../hooks/useAxiox/useAxiox";
 
 
 const Cart = () => {
+    const [cart] = useCart()
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+    const axiosSeciour = useAxiox()
+    const handleDelete = id => {
+        axiosSeciour.delete(`carts/${id}`)
+            .then(res => {
+                console.log(res);
+            })
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                // Swal.fire({
+                //     title: "Deleted!",
+                //     text: "Your file has been deleted.",
+                //     icon: "success"
+                // });
+            }
+        });
+    }
     return (
         <div>
-            <h2 className="text-6xl">My Cart</h2>
+            <div className="flex justify-between">
+                <h2 className="text-4xl">Total Order:{cart.length}</h2>
+                <h2 className="text-4xl">Price:${totalPrice}</h2>
+                <button className="btn btn-outline">Pay</button>
+            </div>
+            {/* for table */}
+            <div className="overflow-x-auto ">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Item image</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            cart.map((item, index) =>
+                                <tr key={item._id}>
+                                    <th>{index + 1}</th>
+                                    <img className="h-10 w-10 rounded" src={item.image} alt="" />
+                                    {/* <td>{item.email}</td> */}
+                                    <td>{item.name}</td>
+                                    <td>${item.price}</td>
+                                    <button onClick={() => handleDelete(item._id)} className="btn btn-lg btn-ghost"><td><MdDelete className="text-red-500 text-2xl"></MdDelete></td></button>
+                                </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/pages/Dashbord/Dashboard.jsx
+++ src/pages/Dashbord/Dashboard.jsx
@@ -1,16 +1,32 @@
 import { NavLink, Outlet } from "react-router-dom";
+import { FaBookmark, FaCalendar, FaHome, FaRadiation, FaShoppingBag, FaShoppingCart, } from 'react-icons/fa';
+import { MdMail, MdMenu } from "react-icons/md";
+
 
 const Dashboard = () => {
     return (
         <div className="flex">
             {/* dashboard side bar */}
             <div className="w-64 min-h-full bg-orange-400">
-                <ul className="menu">
-                    <li><NavLink to={'deshboard/cart'}>My Cart</NavLink></li>
+                <ul className="menu p-4">
+                    <li><NavLink to={'/dashboard/userHome'}><FaHome></FaHome>Home</NavLink></li>
+                    <li><NavLink to={'/dashboard/cart'}><FaShoppingCart></FaShoppingCart>My Cart</NavLink>
+                    </li>
+                    <li><NavLink to={'/dashboard/reservation'}><FaCalendar></FaCalendar>Reservation</NavLink></li>
+                    <li><NavLink to={'/dashboard/rating'}><FaRadiation></FaRadiation>Reservation</NavLink></li>
+                    <li><NavLink to={'/dashboard/booking'}><FaBookmark></FaBookmark>Booking</NavLink></li>
+                    <div className="divider"></div>
+                    <li><NavLink to={'/'}><FaHome></FaHome>Home</NavLink></li>
+                    <li><NavLink to={'/order/salad'}><MdMenu >
+                    </MdMenu>Menu</NavLink></li>
+                    <li><NavLink to={'/dashboard/shop'}><FaShoppingBag >
+                    </FaShoppingBag>Shop</NavLink></li>
+                    <li><NavLink to={'/dashboard/contact'}><MdMail >
+                    </MdMail>Contact</NavLink></li>
                 </ul>
             </div>
             {/* dashboard content */}
-            <div className="flex-1">
+            <div className="flex-1 p-8">
                 <Outlet></Outlet>
             </div>
         </div>
